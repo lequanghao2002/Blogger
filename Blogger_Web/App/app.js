78c31351a47093e026846ac66e803e4eeadec686
Quang Hào <haolq2002@gmail.com>
2023-06-02 22:34:33 +0700

    Triển khai cơ chế routing SPA

---
+++ Blogger_Web/App/app.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../assets/admin/libs/angular-1.8.2/angular.js" />
+
+(function () {
+    angular.module('Blogger',
+        [
+            'BloggerCategory',
+            'BloggerCommon'
+        ]).config(config);
+
+    // Hai inject này nằm trong ui-router
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                // Đường dẫn vào view
+                url: '/admin',
+                // Vào view này nó tự nhận và sử dụng ctrl ở dưới
+                templateUrl: '/app/components/home/homeView.html',
+                // Không khai báo homeController thì bị lỗi
+                controller: 'homeController'
+            });
+
+        $urlRouterProvider.otherwise('/admin');
+    };
+})();
